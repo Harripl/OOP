5ddc5a25f0bde852aebf262c8d7dbdc37f9e91b1
Harripl <80278037+Harripl@users.noreply.github.com>
2021-03-18 12:19:28 -0400

    Add files via upload

---
+++ Class Example/src/com/asc/oop/main/RandomExampleApp.java
@@ -0,0 +1,22 @@
+package com.asc.oop.main;
+
+import java.util.Scanner;
+
+public class RandomExampleApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the program \n Please enter your name in the following format (first last)");
+		String inpt = "";
+		String fName = "";
+		String lName = "";
+		
+		Scanner sc = new Scanner(System.in);
+		inpt = sc.nextLine();
+		//Lomas Harripersad
+		fName = inpt.substring(0,4);
+		System.out.println("First name is: "+ fName );
+
+	}
+
+}
